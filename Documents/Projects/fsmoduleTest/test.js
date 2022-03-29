3ece7d684e013d5469e015950563f089939af063
Okoye Victor <okoyeebuka64@gmail.com>
2022-03-30 00:20:21 +0100

    My first commit

--- Documents/Projects/fsmoduleTest/test.js
+++ Documents/Projects/fsmoduleTest/test.js
@@ -1,12 +1,6 @@
 const fs = require('fs')
-//console.log(fs);
-//fs.open('test.txt','w+', function(err, f) {
-// console.log('This is a demo text');
-//})
-
-
 fs.open('demo.txt', 'w+', function (err, f) {
-    fs.write(f , 'demo text', function (err) {
+    fs.write(f, 'Testing demo text', function (err) {
            console.log('alright,i am writing to my text file');
            if(err)
            throw err
